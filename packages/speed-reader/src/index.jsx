d389b3859f5dddf576668466b750ff53d7e822dd
Dejan Toteff <deyan8284@gmail.com>
2019-06-21 14:44:44 +0300

    feat@dnd

--- packages/speed-reader/src/index.jsx
+++ packages/speed-reader/src/index.jsx
@@ -1,3 +1,5 @@
+import { defaultTo } from 'rambdax'
+import { takeArguments } from 'string-fn'
 import './style.scss'
 import React from 'react'
 import { render } from 'react-dom'
@@ -5,8 +7,17 @@ import { sentryAnt } from './ants/sentry.js'
 import { SpeedReader } from '../../../../stories/src/SpeedReader/component.js'
 sentryAnt()
 
+const {index, base, progress } = takeArguments(window.location.href)
+const bookIndex = defaultTo(0,index)
+const baseRate = defaultTo(1,base)
+const progressIndex = defaultTo(0,progress)
+
 function Root(){
-  <SpeedReader />
+  return <SpeedReader 
+    bookIndex={bookIndex}
+    baseRate={baseRate}
+    progressIndex={progressIndex}
+  />
 }
 
 render(<Root />, document.getElementById('root'))
