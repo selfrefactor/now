760e4351abedaa166f395f27f883c3c283794340
Dejan Toteff <deyan8284@gmail.com>
2019-09-25 23:58:59 +0300

    chore@dnd

--- packages/speed-reader/src/index.jsx
+++ packages/speed-reader/src/index.jsx
@@ -6,7 +6,7 @@ import { SpeedReaderComplete } from '../stories/apps/SpeedReader/complete.js'
 sentryAnt()
 
 function Root(){
-  return <SpeedReaderComplete production={true} />
+  return <SpeedReaderComplete />
 }
 
 render(<Root />, document.getElementById('root'))
