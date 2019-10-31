c0a1bc663f2aedc8e74e118a5781ce3b4a1ad668
Dejan Toteff <deyan8284@gmail.com>
2019-11-01 01:02:33 +0200

    feat: update deps and upgrade rambdax

--- packages/rambda-repl-app/files/lintAll.js
+++ packages/rambda-repl-app/files/lintAll.js
@@ -1,8 +1,7 @@
 const klawSync = require('klaw-sync')
 const { exec } = require('child_process')
 
-const { log } = require('log')
-const { composeAsync, mapFastAsync, prop, takeLast } = require('rambdax')
+const { mapFastAsync, prop, takeLast } = require('rambdax')
 const { resolve, parse } = require('path')
 
 const execCommand = command =>
@@ -16,40 +15,36 @@ const execCommand = command =>
     proc.stdout.on('error', err => reject(err))
   })
 
-async function lintAll (listOfPaths) {
-  await mapFastAsync( async ( filePath ) =>{
+async function lintAll (listOfPaths){
+  await mapFastAsync(async filePath => {
     const { base } = parse(filePath)
-    log('lint start',base, `icon.tag=${base}`)
+    console.log('lint start', base, `icon.tag=${ base }`)
     await execCommand(`tslint ${ filePath } --fix`)
-    
+
     await execCommand(`tsfmt -r ${ filePath }`)
-    log('format over',takeLast(20, filePath), `icon.tag=${base}`)
+    console.log('format over', takeLast(20, filePath), `icon.tag=${ base }`)
   })(listOfPaths)
 
-  return 
 }
-
 
 async function lintAllSrcFolder(){
   try {
     const allFilesRaw = klawSync(
       resolve(__dirname, '../src'),
-      {
-        nodir  : true,
-      }
+      { nodir : true }
     )
-  
+
     const allFiles = allFilesRaw.map(prop('path'))
-  
-    const allowedFiles = allFiles.filter(filePath => 
+
+    const allowedFiles = allFiles.filter(filePath =>
       !filePath.includes('__snapshots__') &&
-      ( filePath.endsWith('ts') || filePath.endsWith('tsx') )
+      (filePath.endsWith('ts') || filePath.endsWith('tsx'))
     )
-  
+
     await lintAll(allowedFiles)
-  
-    log('', 'success')
-  } catch (e) {
+
+    console.log('', 'success')
+  } catch (e){
     console.log(e)
   }
 }
