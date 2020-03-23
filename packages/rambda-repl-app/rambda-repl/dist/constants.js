089fcd11c3df18a5c49668bfada7f720ba0e4c9b
Dejan Toteff <deyan8284@gmail.com>
2020-03-24 00:40:23 +0200

    chore@dnd

--- packages/rambda-repl-app/rambda-repl/dist/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REPL_URL = 'https://rambda.now.sh';
-//# sourceMappingURL=constants.js.map
--- packages/rambda-repl-app/rambda-repl/dist/debug.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require("./");
-void function debug() {
-    const result = _1.rambdaREPL('foo');
-    console.log(result);
-}();
-//# sourceMappingURL=debug.js.map
