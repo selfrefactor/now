fd04a9be6812e00cb6cfaf466869d6cf5d0829be
Dejan Toteff <deyan8284@gmail.com>
2020-03-29 17:53:28 +0300

    feat: basic grid

--- packages/joke-maker/src/app/app.component.ts
+++ packages/joke-maker/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import {Component} from '@angular/core'
+import { range } from 'rambdax'
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'joke-maker'
+  words: string[] = range(0,6).map(String)
+  ngOnInit(){
+    console.log(this.words)
+  }
 }
--- packages/joke-maker/src/app/app.module.ts
+++ packages/joke-maker/src/app/app.module.ts
@@ -3,10 +3,14 @@ import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {AppComponent} from './app.component'
+import {CellComponent, GridComponent, SubGridComponent} from 'grid-fn'
 
 @NgModule({
   declarations: [
     AppComponent,
+    GridComponent,
+    CellComponent,
+    SubGridComponent,
   ],
   imports: [
     BrowserModule,
