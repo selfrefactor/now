2fad6f54b736880b091f53bbbf178f7131493b75
Dejan Totev <dejan.totev@psinteractive.io>
2021-01-17 23:23:47 +0200

    chore@dnd

--- packages/lambdas-fn/api/api.js
+++ packages/lambdas-fn/api/api.js
@@ -1,11 +1,13 @@
 const data = require('./db.json')
- 
-module.exports = (req, res) => {
-  const fallback = res.status(200).json({rows:[], auth: 'incorrect password'})
 
-  if(!req.body) return fallback
-  if(!req.body.password) return fallback
-  if(req.body.password !== process.env.password) return fallback
+const handler = (req, res) => {
+  const fallback = () => res.status(200).json({rows:[], auth: 'incorrect password'})
+
+  if(!req.body) return fallback()
+  if(!req.body.password) return fallback()
+  if(req.body.password !== process.env.password) return fallback()
 
   res.status(200).json(data)
 }
+
+module.exports = handler
