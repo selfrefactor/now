fb476ed1fe758b0e52b4174c4e000bb84770eb84
Deyan Totev <deyan.totev.extern@seven.one>
2023-05-17 23:08:55 +0300

    chore@small

--- packages/lambdas-fn/index.js
+++ packages/lambdas-fn/index.js
@@ -1,19 +1,41 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const data = require('./src/db.json')
 
-app.get('/', (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader('Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+  if (req.method === 'OPTIONS'){
+    res.status(200).end()
+
+    return
+  }
+
+  return await fn(req, res)
+}
+
+const fn = (req, res) => {
   res.json({
     status  : 200,
-    message : 'Get data11 has successfully',
+    message : JSON.stringify(
+      data, null, 2
+    ),
   })
-})
-app.post('/', (req, res) => {
-  res.json({
-    status  : 200,
-    message : 'Get data33 has successfully',
-  })
-})
+}
+app.get('/', allowCors(fn))
+app.get('/female-stars-list', allowCors(fn))
 
-app.listen(process.env.PORT || 3000);
+// app.post('/', (req, res) => {
+//   res.json({
+//     status  : 200,
+//     message : 'Get data33 has successfully',
+//   })
+// })
 
-module.exports = app;
+app.listen(process.env.PORT || 3000)
+
+module.exports = app
