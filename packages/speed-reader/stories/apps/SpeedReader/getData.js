edb5db4a7af09d71b3d3d0c7f22a50841cb940b6
Deyan Totev <deyan.totev.extern@seven.one>
2023-06-01 10:48:46 +0300

    feat@cache request

--- packages/speed-reader/stories/apps/SpeedReader/getData.js
+++ packages/speed-reader/stories/apps/SpeedReader/getData.js
@@ -1,8 +1,25 @@
 import { API_URL } from '../../constants'
 import { post } from 'axios'
 
-export async function getData(bookIndex, password){
-  const { data } = await post(`${ API_URL }/speed-reader/`, { id : bookIndex, password })
+function saveToLocalStorage(data, bookIndex){
+  localStorage.setItem(bookIndex, JSON.stringify(data))
+}
 
-  return data.filter(x => x.trim().length > 0)
+function getFromLocalStorage (bookIndex){
+  let data = localStorage.getItem(bookIndex)
+  return data ? JSON.parse(data) : null
+}
+
+export async function getData(bookIndex, password){
+  let bookData
+  try {
+    const { data } = await post(`${ API_URL }/speed-reader/`, { id : bookIndex, password })
+    bookData = data
+    saveToLocalStorage(data, bookIndex)
+  } catch (error) {
+    bookData = getFromLocalStorage(bookIndex)
+    if(bookData) console.log( 'Using local data')
+  }
+  if(!bookData) throw new Error('No data')
+  return bookData.filter(x => x.trim().length > 0)
 }
