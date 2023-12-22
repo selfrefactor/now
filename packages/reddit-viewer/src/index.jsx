2a1bb84d778b701afc33f21c7577cc19fbb6fa5a
Deyan Totev <deyan.totev.extern@seven.one>
2023-12-22 21:36:15 +0200

    chore@small

---
+++ packages/reddit-viewer/src/index.jsx
@@ -0,0 +1,12 @@
+import './style.scss'
+import React from 'react'
+import { render } from 'react-dom'
+import { sentryAnt } from './ants/sentry.js'
+import { RedditViewer } from '../../stories/apps/RedditViewer/component'
+sentryAnt()
+
+function Root(){
+  return <RedditViewer />
+}
+
+render(<Root />, document.getElementById('root'))
