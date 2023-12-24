72e5d17de6cb31e216668ea354660de38e83fb62
Deyan Totev <deyan.totev.extern@seven.one>
2023-12-24 15:27:21 +0200

    feat@favicon

--- packages/select-correct-word/src/index.tsx
+++ packages/select-correct-word/src/index.tsx
@@ -1,8 +1,7 @@
 import './App.scss';
-import {render} from 'react-dom';
 import { App } from './App';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
 
-render(
-        <App />,
-  document.getElementById('root'),
-);
