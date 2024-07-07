ab62e591e00d99b505eeed3d7ca0e8df43b3f7b6
Deyan Totev <deyan8284@gmail.com>
2024-07-07 23:56:02 +0300

    feat@key route

--- packages/lambdas-fn/index.js
+++ packages/lambdas-fn/index.js
@@ -4,15 +4,19 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 const { readBook } = require('./src/read-book')
 const REQUEST_PASSWORD = true
-// const REQUEST_PASSWORD = false
 var jsonParser = bodyParser.json()
 
+let API_PASSWORD_KEY = process.env.API_PASSWORD_KEY
+
 const dataRoute = async (req, res) => {
+  if(!API_PASSWORD_KEY) return res.status(401).json({
+    status  : 401,
+    message : 'missing key error',
+  })
   const bookId = req.params.id;
   const password = req.body.password;
-  const vercelPassword = process.env.API_PASSWORD_KEY || '123456'
   if(
-    password !== vercelPassword &&
+    password !== API_PASSWORD_KEY &&
     REQUEST_PASSWORD
   ) return res.status(401).json({
     status  : 401,
@@ -28,6 +32,28 @@ const dataRoute = async (req, res) => {
     ),
   })
 }
+const secretKeyRoute = async (req, res) => {
+  if(!API_PASSWORD_KEY) return res.status(401).json({
+    status  : 401,
+    message : 'missing key error',
+  })
+  const secretKeyRouteId = req.params.id;
+  const password = req.body.password;
+  if(
+    password !== API_PASSWORD_KEY &&
+    REQUEST_PASSWORD
+  ) return res.status(401).json({
+    status  : 401,
+    message : 'Unauthorized',
+  })
+
+  return res.json({
+    status  : 200,
+    message : JSON.stringify(
+      {NEWS_API_KEY: process.env.NEWS_API_KEY, secretKeyRouteId}, null, 2
+    ),
+  })
+}
 
 const options = [
   cors({
@@ -40,6 +66,7 @@ const options = [
 
 app.use(options);
 app.post('/books/:id', jsonParser, dataRoute)
+app.post('/secrets/:id', jsonParser, secretKeyRoute)
 
 app.listen(process.env.PORT || 3000)
 
